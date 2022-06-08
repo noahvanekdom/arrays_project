7ea7dd1df24da0f8f754a0ac6ad21cacac64b037
Noah van Ekdom <noahvanekdom@gmail.com>
2022-06-08 08:50:22 -0600

    Initial commit

---
+++ arrays.rb
@@ -0,0 +1,4 @@
+batter_names = ["Juan Soto", "Mike Trout", "Mookie Betts", "Shohei Ohtani"]
+batter_abs = [109, 94, 119, 124]
+batter_ops = [0.784, 1.023, 0.897, 0.793]
+batter_rhanded = [false, false, false, false]
