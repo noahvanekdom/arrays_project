392a83ab20019da170d0211f5df144f0cc70e13f
Noah van Ekdom <noahvanekdom@gmail.com>
2022-06-08 09:21:03 -0600

    Take first steps to reorganize array

--- arrays.rb
+++ arrays.rb
@@ -3,11 +3,23 @@ abs = [109, 94, 119, 124]
 ops = [0.784, 1.023, 0.897, 0.793]
 rhanded = [false, false, false, false]
 rhanded = [false, true, false, false]
-# this will remove the final variable from the names array, leaving only Juan Mike and Mookie
+# this will remove the final variable from the rhanded array
 rhanded.pop
 # this will add the value 131 as the 5th element in the abs array
 abs.push(131)
-# this will retrieve/print/show the first element in the abs array, so 109
+# this will retrieve/print/show the first element in the ops array, so 109, then remove it
 ops.shift
-#this will all the name Keibert Ruiz to the names array in the first position -- names(0)
+#this will add the name Keibert Ruiz to the names array in the first position -- names(0)
 names.unshift("Keibert Ruiz")
+
+# now, we have arrays with different lengths, and statistics not matching up in which order of the array they are present in. Let's fix that.
+
+rhanded.insert(3, false)
+names.shift
+ops.insert(0, 109)
+abs.delete_at(4)
+
+names
+abs
+ops
+rhanded
